20e1e53ecc0be7a26fadf8e35932787fdf867a0c
Robbie <robert.sinclair.15@ucl.ac.uk>
2022-12-23 22:55:53 +0000

    port name

--- node.js
+++ node.js
@@ -3,7 +3,7 @@ const express = require('express')
 const http = require('http')
 const fs = require('fs')
 const server = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 // Listen on Port 5000
 server.listen(port, () => console.info(`App listening on port ${port}`))
