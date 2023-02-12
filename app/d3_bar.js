0a2f1f8c0d3c27b6d4757fe4c5da97c86551d36a
Robbie <robert.sinclair.15@ucl.ac.uk>
2023-02-12 21:00:25 +0000

    overflow issues

--- app/d3_bar.js
+++ app/d3_bar.js
@@ -40,8 +40,8 @@ function election_bar(constit) {
 
   // set the dimensions and margins of the graph
   var margin = {top: 10, right: 10, bottom: 10, left: 10},
-      width = 400 - margin.left - margin.right,
-      height = 300 - margin.top - margin.bottom;
+      width = 300 - margin.left - margin.right,
+      height = 200 - margin.top - margin.bottom;
 
   // append the svg object to the body of the page
   var svg = d3.select('#election-bar')
